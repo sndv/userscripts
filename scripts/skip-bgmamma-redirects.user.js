9e7ef16625b5aab268a7ca3d7dcf6e844f95498a
sndv <sndv@mailbox.org>
2021-05-27 22:51:22 +0300

    Minor coding style improvements

--- scripts/skip-bgmamma-redirects.user.js
+++ scripts/skip-bgmamma-redirects.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/sndv/userscripts
 // @match       https://www.bg-mamma.com/
 // @grant       none
-// @version     0.2.1
+// @version     0.2.2
 // @author      sndv
 // @description Change bg-mamma.com/url.php links to go directly to their destination to prevent adding unwanted coupon codes and tracking
 // @run-at      document-end
@@ -17,17 +17,17 @@ const URL_PARAM_NAME = "u";
 // const LINK_CSS_SELECTOR = "a.url-content";
 const LINK_CSS_SELECTOR = "a";
 
-var fixedCount = 0;
+let fixedCount = 0;
 
 document.querySelectorAll(LINK_CSS_SELECTOR).forEach(el => {
   try {
     if (el.href.includes(REDIRECT_URL_IDENTIFIER)) {
-      const url = new URL(el.href);
-      const targetEncoded = url.searchParams.get(URL_PARAM_NAME);
+      let url = new URL(el.href);
+      let targetEncoded = url.searchParams.get(URL_PARAM_NAME);
       if (targetEncoded) {
-        const targetFull = decodeURIComponent(atob(targetEncoded));
+        let targetFull = decodeURIComponent(atob(targetEncoded));
         if (targetFull.includes(LINK_SPLIT_PATTERN)) {
-          const target = targetFull.split(LINK_SPLIT_PATTERN, 1)[0];
+          let target = targetFull.split(LINK_SPLIT_PATTERN, 1)[0];
           // Rewrite the link target
           el.setAttribute("href", target);
           // Remove the onclick javascript which triggers the reddirect (is it needed?)
